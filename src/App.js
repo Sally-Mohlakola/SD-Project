96d0a2487bfa6ef163d6887c536bf5271fc9dbc5
Sally Mohlakola <sally.mohlakola@gmail.com>
2025-04-12 11:12:57 +0200

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {Auth} from "./components/auth";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {Home} from './components/home';
 
-
 function App() {
  return (<section className="App"><Auth/></section>);
   
