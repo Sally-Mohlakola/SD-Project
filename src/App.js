8a0c1bdf8a05f1983fad15fb7d1d923ebbf6a1a9
HlulaniB <baloyihlulani91@gmail.com>
2025-04-20 01:19:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
 
-   return (
+   return ( 
       <section className="container">
       <Routes>
          <Route path ="/" element= {<Auth/>}/>
