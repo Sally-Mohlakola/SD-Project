67c456c23796ea5aa3780cbadffdde41cf45c13e
HlulaniB <baloyihlulani91@gmail.com>
2025-05-16 00:46:02 +0200

    Update updateproducts.js

--- src/components/updateproducts.js
+++ src/components/updateproducts.js
@@ -2,7 +2,7 @@ import { docs,doc,where ,collection,query,getDocs,updateDoc} from "firebase/fire
 import { db,storage } from "../config/firebase";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-
+import '../styles/updateproducts.css'
 import { useShopId } from "./userinfo";
 import { v4 as uuidv4 } from 'uuid';
 import { ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
@@ -166,35 +166,61 @@ const Setnewquantity=async(e)=>{
 
 
 return(
-  <section>
-      <h1>Update product</h1>
-      <form onSubmit={Name} style={{ display: "flex", alignItems: "center", gap: "20px", marginBottom: "10px" }}>
+  <section className="Body">
+    <section className="left">
+
+  <h1>Update product</h1>
+  <button onClick={Back}>← Back</button>
+      </section>
+      <section className="right">
+      <form onSubmit={Name} >
+        <section className="form1">
           <label>Name of product:</label>
           <input type="text" required value={productname} onChange={(e)=>setproductname(e.target.value)} />
+          </section>
+          <section className="form2">
           <button type="submit">Update product name</button>
+          </section>
       </form>
       
-      <form onSubmit={Description} style={{ display: "flex", alignItems: "center", gap: "20px", marginBottom: "10px" }}>
+      <form onSubmit={Description} >
+      <section className="form1">
           <label>Description of the product:</label>
           <textarea type="text" required  rows="6" cols="-40"value={description} onChange={(e)=>setdescription(e.target.value)} ></textarea>
+          </section>
+          <section className="form2">
           <button type="submit">Update description</button>
+          </section>
       </form>
-      <form  onSubmit={Price} style={{ display: "flex", alignItems: "center", gap: "20px", marginBottom: "10px" }}>
+      <form  onSubmit={Price} >
+      <section className="form1">
           <label>Price:</label>
           <input type="number" min="1" max = "999" required value={price} onChange={(e)=>setprice(e.target.value)} />
+          </section>
+          <section className="form2">
           <button type="submit">Update price</button>
+          </section>
       </form>
      
-      <form onSubmit={Setnewquantity} style={{ display: "flex", alignItems: "center", gap: "20px", marginBottom: "10px" }}>
+      <form onSubmit={Setnewquantity} >
+      <section className="form1">
           <label>Set new quantity:</label>
           <input type="number" min = "1" max="999" required value={newquantity} onChange={(e)=>setnewquantity(e.target.value)} />
+          </section>
+          <section className="form2">
           <button type="submit">set quantity</button>
+          </section>
       </form>
-      <form onSubmit={Image}  style={{ display: "flex", alignItems: "center", gap: "20px", marginBottom: "10px" }}>
+      <form onSubmit={Image}  >
+      <section className="form1">
       <input type="file" id="image" accept="image/*" onChange={(e) => setimage(e.target.files[0])}/> 
+      </section>
+      <section className="form2">
       <button>Change image</button>
+      </section>
       </form>
-      <button onClick={Back}>← Back</button>
+      </section>
+      
       
   </section>
 );
